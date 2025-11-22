32bcfa3612efa542237e1667062340a66a8b3d85
muhammadhamza-dotcom <std_28346@iobm.edu.pk>
2025-11-22 18:33:45 +0500

    google analytics enabled

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Syne } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -23,6 +24,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-QL0ST0WQ7V"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-QL0ST0WQ7V');
+        `}
+      </Script>
       <body className={`${syne.variable} antialiased`}>
         {/* Apply Syne globally */}
         <div className="font-syne">
